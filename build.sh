6392c5325acab646f19b7a13d75ab024a7b29cf6
Michael Dekker <git@michaeldekker.nl>
2017-05-09 18:30:47 +0200

    Remove PEAR XML Parser dependency

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+CWD_BASENAME=${PWD##*/}
+
+FILES+=("${CWD_BASENAME}.php")
+FILES+=("index.php")
+FILES+=("init.php")
+FILES+=("logo.gif")
+FILES+=("logo.png")
+FILES+=("Readme.md")
+FILES+=("controllers/**")
+FILES+=("entities/**")
+FILES+=("install/**")
+FILES+=("translations/**")
+FILES+=("views/**")
+
+
+MODULE_VERSION="$(sed -ne "s/\\\$this->version *= *['\"]\([^'\"]*\)['\"] *;.*/\1/p" ${CWD_BASENAME}.php)"
+MODULE_VERSION=${MODULE_VERSION//[[:space:]]}
+ZIP_FILE="${CWD_BASENAME}/${CWD_BASENAME}-v${MODULE_VERSION}.zip"
+
+echo "Going to zip ${CWD_BASENAME} version ${MODULE_VERSION}"
+
+cd ..
+for E in "${FILES[@]}"; do
+  find ${CWD_BASENAME}/${E}  -type f -exec zip -9 ${ZIP_FILE} {} \;
+done
